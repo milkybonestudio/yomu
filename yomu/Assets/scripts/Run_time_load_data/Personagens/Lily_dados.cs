f3e9c1879533c6a06eb19742d3dd3fe7700ead28
milkybonestudio <milkybonestudio@gmail.com>
2024-06-02 22:06:18 -0300

    Update Lily_dados.cs

--- yomu/Assets/scripts/Run_time_load_data/Personagens/Lily_dados.cs
+++ yomu/Assets/scripts/Run_time_load_data/Personagens/Lily_dados.cs
@@ -1,19 +1,46 @@
 using UnityEngine;
 using System;
 
+
 // passar para o geral depois
-
-public enum Lily_update_periodo {
-
-        NULL = 0,
-    
-}
 
 public enum Lily_update_run_time {
 
-        NULL = 0,
+        NULL,
+        DEFAULT,
     
 }
+public enum Lily_update_periodo {
+
+        NULL,
+        DEFAULT,
+    
+}
+
+public enum Lily_update_dia {
+
+        NULL,
+        DEFAULT,
+    
+}
+
+public enum Lily_update_semana {
+
+        NULL,
+        DEFAULT,
+    
+}
+
+
+public enum Lily_update_mes {
+
+        NULL,
+        DEFAULT,
+    
+}
+
+
+
 
 /*
     ** todos tem somente 1 byte
@@ -43,18 +70,23 @@ public enum Index_updates{
      Fazer outro teste ( ;-; ) Fazer com +- 50mb dll. se der certo fazer com static => nao precisa construir objetos  
      Na realidade se os metodos forem objetos eu vou usar ainda menos espaço?
 
+     os updates nao vao ser descartados com facilidade. pegar outro => tem tempo => pode criar instancia
+
      talvez eu só seja retardado e tem algum loop no meio?
 */
 
-public class Construtor_update_run_time {
+public class Lily_construtor_update_run_time {
 
-    
     public Action Pegar( Lily_update_tun_time _nome_update ){
 
             switch( _nome_update ){
+                            // null => personagem nao ativo
+                            // DEFAULT => normal personagem 
+                            // os 2 podem ser null
 
-                case Lily_update_tun_time.NULL : return new Run_time_DEFAULT().fn;
+                case Lily_update_tun_time.NULL : return null;
                 case Lily_update_tun_time.DEFAULT : return new Run_time_DEFAULT().fn;
+                default : throw new Exception( "tipo update nao foi encontrado, veio:" + update_run_time );
                     
             }
 
@@ -68,7 +100,7 @@ public class  Run_time_DEFAULT {
 
     public void fn() {
 
-            // aqui 
+            // aqui vai estar a logica em sim
 
         
 
@@ -76,32 +108,31 @@ public class  Run_time_DEFAULT {
     }
         
 }
-    
+
 
 
 public class Lily_dados {
 
-    public 
+        public Lily_construtor_update_run_time construtor_update_run_time = new Lily_construtor_update_run_time();
+        public Lily_construtor_update_periodo construtor_update_run_periodo = new Lily_construtor_update_periodo();
+        public Lily_construtor_update_dia construtor_update_dia = new Lily_construtor_update_dia();
+        public Construtor_update_semana construtor_update_semana = new Lily_construtor_update_semana();
+        public Construtor_update_mes construtor_update_mes = new Lily_construtor_update_mes();
 
-    public void Pegar_dados( Personagem _lily ){
+        
 
-            byte[] dados_para _iniciar = _lily.dados_updates;
-
-            Lily_update_run_time update_run_time = ( Lily_update_run_time ) dados_para_iniciar[ Index_updates.run_time ];
-            _lily.Update_run_time = Lily_construtor_update_run_time();
+        public void Pegar_dados(){
+        
+                Personagem lily = Controlador_personagens.Pegar_instancia().Pegar_personagem( Personagem_nome.Lily );
             
+                byte[] dados_para _iniciar = Lily.dados_updates;
+                
+                Lily_update_run_time update_run_time = ( Lily_update_run_time ) dados_para_iniciar[ Index_updates.run_time ];
+                Lily.Update_run_time = Construtor_update_run_time( update_run_time );
+                
+                
+                return;
 
-            switch( update_run_time ) {
-                    // colocar updates 
-                case Lily_update_run_time.NULL : _lily.Update_run_time = null;  break;
-                default : throw new Exception( "tipo update nao foi encontrado, veio:" + update_run_time );
-                    
-            }
-
-        
-        
-            return;
-        
-    }
+        }
 
 }
