b734239d2a99205309a2c4853fec56dd9eef69e2
milkybonestudio <milkybonestudio@gmail.com>
2024-06-02 22:30:20 -0300

    Update Lily_dados.cs

--- yomu/Assets/scripts/Run_time_load_data/Personagens/Lily/Lily_dados.cs
+++ yomu/Assets/scripts/Run_time_load_data/Personagens/Lily/Lily_dados.cs
@@ -31,6 +31,33 @@ using System;
      talvez eu só seja retardado e tem algum loop no meio?
 */
 
+
+public class Lily_dados {
+
+        public Lily_construtor_update_run_time construtor_update_run_time = new Lily_construtor_update_run_time();
+        public Lily_construtor_update_periodo construtor_update_run_periodo = new Lily_construtor_update_periodo();
+        public Lily_construtor_update_dia construtor_update_dia = new Lily_construtor_update_dia();
+        public Construtor_update_semana construtor_update_semana = new Lily_construtor_update_semana();
+        public Construtor_update_mes construtor_update_mes = new Lily_construtor_update_mes();
+
+        
+
+        public void Pegar_dados(){
+        
+                Personagem lily = Controlador_personagens.Pegar_instancia().Pegar_personagem( Personagem_nome.Lily );
+            
+                byte[] dados_para _iniciar = Lily.dados_updates;
+                
+                Lily_update_run_time update_run_time = ( Lily_update_run_time ) dados_para_iniciar[ Index_updates.run_time ];
+                Lily.Update_run_time = Construtor_update_run_time( update_run_time );
+                
+                
+                return;
+
+        }
+
+}
+
 public class Lily_construtor_update_run_time {
 
     public Action Pegar( Lily_update_tun_time _nome_update ){
