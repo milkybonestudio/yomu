f1074a32dea1fb7af1996b97abd3a35bba75fedb
milkybonestudio <milkybonestudio@gmail.com>
2024-06-02 22:26:17 -0300

    Update Lily_dados.cs

--- yomu/Assets/scripts/Run_time_load_data/Personagens/Lily_dados.cs
+++ yomu/Assets/scripts/Run_time_load_data/Personagens/Lily_dados.cs
@@ -4,42 +4,6 @@ using System;
 
 // passar para o geral depois
 
-public enum Lily_update_run_time {
-
-        NULL,
-        DEFAULT,
-    
-}
-public enum Lily_update_periodo {
-
-        NULL,
-        DEFAULT,
-    
-}
-
-public enum Lily_update_dia {
-
-        NULL,
-        DEFAULT,
-    
-}
-
-public enum Lily_update_semana {
-
-        NULL,
-        DEFAULT,
-    
-}
-
-
-public enum Lily_update_mes {
-
-        NULL,
-        DEFAULT,
-    
-}
-
-
 
 
 /*
@@ -48,15 +12,7 @@ public enum Lily_update_mes {
 
 */
 
-public enum Index_updates{
 
-    run_time, 
-    periodo, 
-    dia,
-    semana, 
-    mes,
-    
-}
 
 /*
 
@@ -111,28 +67,4 @@ public class  Run_time_DEFAULT {
 
 
 
-public class Lily_dados {
 
-        public Lily_construtor_update_run_time construtor_update_run_time = new Lily_construtor_update_run_time();
-        public Lily_construtor_update_periodo construtor_update_run_periodo = new Lily_construtor_update_periodo();
-        public Lily_construtor_update_dia construtor_update_dia = new Lily_construtor_update_dia();
-        public Construtor_update_semana construtor_update_semana = new Lily_construtor_update_semana();
-        public Construtor_update_mes construtor_update_mes = new Lily_construtor_update_mes();
-
-        
-
-        public void Pegar_dados(){
-        
-                Personagem lily = Controlador_personagens.Pegar_instancia().Pegar_personagem( Personagem_nome.Lily );
-            
-                byte[] dados_para _iniciar = Lily.dados_updates;
-                
-                Lily_update_run_time update_run_time = ( Lily_update_run_time ) dados_para_iniciar[ Index_updates.run_time ];
-                Lily.Update_run_time = Construtor_update_run_time( update_run_time );
-                
-                
-                return;
-
-        }
-
-}
