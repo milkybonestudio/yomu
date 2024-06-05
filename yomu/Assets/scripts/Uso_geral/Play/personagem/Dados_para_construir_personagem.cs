9297ea89aa91140ad22e1c4c8e8774623c2afec2
milkybonestudio <milkybonestudio@gmail.com>
2024-06-05 18:15:46 -0300

    Update Dados_para_construir_personagem.cs

--- yomu/Assets/scripts/Uso_geral/Play/personagem/Dados_para_construir_personagem.cs
+++ yomu/Assets/scripts/Uso_geral/Play/personagem/Dados_para_construir_personagem.cs
@@ -1,14 +1,8 @@
 
 
-
-
 public class Dados_para_construir_personagem {
 
-
-    public byte[] estado_mental_info = new byte[ 8 ];
-
     public byte[] dados_gerenciador_estado_mental;
-
         
 }
 
